737f1b904d520871388f4e1df01e0041be72a31b
Mike <mikeramzdesign@gmail.com>
2020-06-09 13:00:18 -0700

    added next

--- src/models/User.js
+++ src/models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', function (next) {
   const user = this
   if (!user.isModified('password')) {
-    return next
+    return next()
   }
 
   bcrypt.genSalt(10, (err, salt) => {
